3ce0094ae958a37ea7585ea6b2d1281f22707295
최민 <rmdwjd76@naver.com>
2024-07-19 16:25:16 +0900

    init : 백엔드 최초 프로젝트

---
+++ BackEnd/RunUs/src/test/java/runus/runus/RunUsApplicationTests.java
@@ -0,0 +1,13 @@
+package runus.runus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
