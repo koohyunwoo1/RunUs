539ff672ab489e3f1331797a7ac14c0862d02ad1
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-08-02 10:55:17 +0900

    [INFRA] feat : 프론트엔드 서버 설정 추가

--- FrontEnd/RunUs-Front/vite.config.js
+++ FrontEnd/RunUs-Front/vite.config.js
@@ -1,19 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:8080",
-        target: "https://i11e103.p.ssafy.io:8001",
-        changeOrigin: true,
-        secure: false,
+// Load environment variables
+dotenv.config();
+
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          // target: "http://localhost:8080",
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
+      port: 3000,
+      host: true
     },
-    port: 3000,
-    host: true
-  },
+  };
 });
