1f8ee030ec80d8a9aeecebf53b670586f2276b9e
jhpark <brianwogus@naver.com>
2024-08-05 10:00:19 +0900

    [FE] fix: QR code 후면카메라 기능

--- FrontEnd/RunUs-Front/src/pages/Running/Team/TeamQR.jsx
+++ FrontEnd/RunUs-Front/src/pages/Running/Team/TeamQR.jsx
@@ -6,7 +6,7 @@ import { UserContext } from "../../../hooks/UserContext";
 
 const TeamQR = () => {
   const [data, setData] = useState("No result");
-  const { addUserToRoom, userData, roomUsers } = useContext(UserContext);
+  const { addUserToRoom, userData } = useContext(UserContext);
 
   const handleScan = (result) => {
     if (result && result.text) {
@@ -95,8 +95,10 @@ const TeamQR = () => {
           onError={handleError}
           onScan={handleScan}
           style={{ width: "100%", height: "100%" }} // 부모 컨테이너에서 높이를 조절
+          facingMode="environment" // 후면 카메라 설정, 가능하지 않으면 기본 카메라 사용
         />
       </div>
+      <p>{data}</p>
     </div>
   );
 };
