671baef4a8858aeff0883272baa33ec67c8cd268
구현우 <kjklovekhw@gmail.com>
2024-08-07 23:34:50 +0900

    [FE] style : LoginHome css 수정

--- FrontEnd/RunUs-Front/src/pages/Home/LogInHome.jsx
+++ FrontEnd/RunUs-Front/src/pages/Home/LogInHome.jsx
@@ -1,35 +1,59 @@
 import React, { useState, useRef, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import "../../styles/Home/LogInHome.css";
 import Header from "../../components/common/Header";
 import ReportItem from "../../components/Report/ReportItem";
 import Button2 from "../../components/common/Button2";
-import SoloProfile from "../../assets/SoloProfile.png";
-import TeamProfile from "../../assets/TeamProfile.png";
+import SoloProfile from "../../assets/SoloProfile2.png";
+import TeamProfile from "../../assets/TeamProfile2.png";
 import axios from "axios";
 import { UserContext } from "../../hooks/UserContext";
 import Weather from "../../components/common/Weather";
+import "../../styles/Home/LoginHomeCustomSwal.css";
 
 const LogInHome = () => {
-  const [showTeamOptions, setShowTeamOptions] = useState(false);
-  const [popupPosition, setPopupPosition] = useState({ top: 0, left: 0 });
   const [distance, setDistance] = useState("0km"); // 기본값
   const teamProfileRef = useRef(null);
   const navigate = useNavigate();
-  const { userData, addUserToRoom } = useContext(UserContext);
+  const { userData } = useContext(UserContext);
 
-  const handleTeamProfileClick = () => {
-    if (teamProfileRef.current) {
-      const rect = teamProfileRef.current.getBoundingClientRect();
-      setPopupPosition({
-        top: rect.top + window.scrollY - rect.height,
-        left: rect.left + window.scrollX,
-      });
-    }
-    setShowTeamOptions(!showTeamOptions);
+  const handleTeamProfileClick = async () => {
+    const result = await Swal.fire({
+      title: "팀 옵션을 선택해주세요 !",
+      html: `
+        <div class="swal2-button-container2">
+          <button id="createTeam" class="swal2-confirm2 swal2-styled2">팀 생성</button>
+          <button id="joinTeam" class="swal2-cancel2 swal2-styled2">팀 입장</button>
+        </div>
+      `,
+      showConfirmButton: false,
+      showCancelButton: false,
+      customClass: {
+        popup: "custom-swal-popup2",
+        title: "custom-swal-title2",
+      },
+      didOpen: () => {
+        document
+          .getElementById("createTeam")
+          .addEventListener("click", handleCreateTeamClick);
+        document
+          .getElementById("joinTeam")
+          .addEventListener("click", handleJoinTeamClick);
+      },
+      willClose: () => {
+        document
+          .getElementById("createTeam")
+          .removeEventListener("click", handleCreateTeamClick);
+        document
+          .getElementById("joinTeam")
+          .removeEventListener("click", handleJoinTeamClick);
+      },
+    });
   };
 
   const handleCreateTeamClick = async () => {
+    Swal.close();
     try {
       const requestBody = {
         ownerUserId: userData.userId,
@@ -48,11 +72,8 @@ const LogInHome = () => {
   };
 
   const handleJoinTeamClick = () => {
+    Swal.close(); // Close the SweetAlert popup
     navigate("/team-QR");
-  };
-
-  const handleSoloProfileClick = () => {
-    navigate("/solo");
   };
 
   const updateDistance = (newDistance) => {
@@ -63,32 +84,18 @@ const LogInHome = () => {
     <div>
       <Header />
       <div className="LoginHome-container">
-        {/* <h1 className="Today_item">RunUs</h1> */}
         <Weather />
         <div className="LoginHomeReportItem">
           <ReportItem onDistanceChange={updateDistance} />
         </div>
         <div className="MainButton-container">
-          <Button2 src={SoloProfile} onClick={handleSoloProfileClick}/>
+          <Button2 src={SoloProfile} onClick={() => navigate("/solo")} />
           <Button2
             src={TeamProfile}
             onClick={handleTeamProfileClick}
             ref={teamProfileRef}
           />
         </div>
-
-        {showTeamOptions && (
-          <div
-            className="team-options-popup"
-            style={{
-              top: popupPosition.top - 10,
-              left: popupPosition.left - 10,
-            }}
-          >
-            <button onClick={handleCreateTeamClick}>팀 생성</button>
-            <button onClick={handleJoinTeamClick}>팀 입장</button>
-          </div>
-        )}
       </div>
     </div>
   );
