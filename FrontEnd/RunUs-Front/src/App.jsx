92a919e004831197938680791780d1b6330a9b9e
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-08-07 15:03:40 +0900

    [FE] fix : Redirect 설정 수정

--- FrontEnd/RunUs-Front/src/App.jsx
+++ FrontEnd/RunUs-Front/src/App.jsx
@@ -22,6 +22,7 @@ import MyPageEdit from "./pages/MyPage/MyPageEdit";
 import CountDown from "./pages/Running/Team/CountDown";
 import TeamCheck from "./pages/Running/Team/TeamCheck";
 import ProtectedRoute from "./components/common/ProtectedRoute";
+import RedirectRoute from "./components/common/RedirectRoute";
 
 import axios from 'axios';
 
@@ -58,23 +59,129 @@ const AppContent = () => {
   return (
     <div>
       <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<LogOutHome />} />
-        <Route path="/home" element={<ProtectedRoute><LogInHome /></ProtectedRoute>} />
-        <Route path="/solo" element={<ProtectedRoute><SoloMode /></ProtectedRoute>} />
-        <Route path="/team-create/:id" element={<ProtectedRoute><TeamCreate /></ProtectedRoute>} />
-        <Route path="/countdown/:id" element={<ProtectedRoute><CountDown /></ProtectedRoute>} />
-        <Route path="/team-QR" element={<ProtectedRoute><TeamQR /></ProtectedRoute>} />
-        <Route path="/report-home" element={<ProtectedRoute><ReportHome /></ProtectedRoute>} />
-        <Route path="/article-home" element={<ProtectedRoute><ArticleHome /></ProtectedRoute>} />
-        <Route path="/article-edit/:id" element={<ProtectedRoute><ArticleEdit /></ProtectedRoute>} />
-        <Route path="/my-page-home" element={<ProtectedRoute><MyPageHome /></ProtectedRoute>} />
-        <Route path="/article-create" element={<ProtectedRoute><ArticleCreate /></ProtectedRoute>} />
-        <Route path="/article-detail/:id" element={<ProtectedRoute><ArticleDetail /></ProtectedRoute>} />
-        <Route path="/my-page-edit" element={<ProtectedRoute><MyPageEdit /></ProtectedRoute>} />
-        <Route path="/team-check/:id" element={<ProtectedRoute><TeamCheck /></ProtectedRoute>} />
-      </Routes>
+          <Route path="/signin" element={
+            <RedirectRoute>
+              <SignIn />
+            </RedirectRoute>
+              
+            } />
+            <Route path="/signup" element={
+              <RedirectRoute>
+                <SignUp />
+              </RedirectRoute>
+              
+            } />
+          <Route path="/" element={
+            <RedirectRoute>
+              <LogOutHome />
+            </RedirectRoute>} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <LogInHome />
+              </ProtectedRoute>
+            }
+          />
+          
+          {/* 로그인 해야 접근 가능한 페이지 */}
+            <Route
+              path="/solo"
+              element={
+                <ProtectedRoute>
+                  <SoloMode />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/team-create/:id"
+              element={
+                <ProtectedRoute>
+                  <TeamCreate />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/countdown/:id"
+              element={
+                <ProtectedRoute>
+                  <CountDown />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/team-QR"
+              element={
+                <ProtectedRoute>
+                  <TeamQR />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/report-home"
+              element={
+                <ProtectedRoute>
+                  <ReportHome />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/article-home"
+              element={
+                <ProtectedRoute>
+                  <ArticleHome />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/article-edit/:id"
+              element={
+                <ProtectedRoute>
+                  <ArticleEdit />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/my-page-home"
+              element={
+                <ProtectedRoute>
+                  <MyPageHome />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/article-create"
+              element={
+                <ProtectedRoute>
+                  <ArticleCreate />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/article-detail/:id"
+              element={
+                <ProtectedRoute>
+                  <ArticleDetail />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/my-page-edit"
+              element={
+                <ProtectedRoute>
+                  <MyPageEdit />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/team-check/:id"
+              element={
+                <ProtectedRoute>
+                  <TeamCheck />
+                </ProtectedRoute>
+              }
+            />
+        </Routes>
     </div>
   );
 };
