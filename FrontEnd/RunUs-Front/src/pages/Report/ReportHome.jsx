bd5c351189359a91a669b8677779fe6b8a126ff4
jhpark <brianwogus@naver.com>
2024-08-08 17:18:01 +0900

    [FE] feat : Report창의 최근활동 top 버튼 만들기

--- FrontEnd/RunUs-Front/src/pages/Report/ReportHome.jsx
+++ FrontEnd/RunUs-Front/src/pages/Report/ReportHome.jsx
@@ -2,6 +2,8 @@ import Header from "../../components/common/Header";
 import "../../styles/Report/ReportHome.css";
 import ReportGraph from "../../components/Report/ReportGraph";
 import ReportItemAll from "../../components/Report/ReportItemAll";
+import TopButton from "../../components/common/TopButton"; // TopButton import
+
 const ReportHome = () => {
   return (
     <div>
@@ -12,6 +14,7 @@ const ReportHome = () => {
       <div>
         <ReportItemAll />
       </div>
+      <TopButton /> {/* TopButton 추가 */}
     </div>
   );
 };
