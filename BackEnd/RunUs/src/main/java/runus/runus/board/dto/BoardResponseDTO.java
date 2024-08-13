2721d0ed875a90afc4ef92c61baa94a1def57ff1
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-08-13 12:44:57 +0900

    [BE] fix : 게시판 정보 경험치 추가

--- BackEnd/RunUs/src/main/java/runus/runus/board/dto/BoardResponseDTO.java
+++ BackEnd/RunUs/src/main/java/runus/runus/board/dto/BoardResponseDTO.java
@@ -1,14 +1,16 @@
 package runus.runus.board.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @Getter
 @Setter
+@Builder
+//@AllArgsConstructor
 public class BoardResponseDTO {
     private int boardId;
     private String title;
@@ -20,8 +22,20 @@ public class BoardResponseDTO {
     private String meetingDay;
     private String nickname;
     private char isDeleted;
+    private Integer exp;
 
-
-
-    // Lombok이 자동으로 게터와 세터를 생성합니다.
+    // 생성자 내부에서 exp가 null인 경우 0으로 설정
+    public BoardResponseDTO(int boardId, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt, int regionId, LocalDateTime meetingTime, String meetingDay, String nickname, char isDeleted, Integer exp) {
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.regionId = regionId;
+        this.meetingTime = meetingTime;
+        this.meetingDay = meetingDay;
+        this.nickname = nickname;
+        this.isDeleted = isDeleted;
+        this.exp = (exp != null) ? exp : 0;  // null일 경우 0으로 설정
+    }
 }
--- BackEnd/RunUs/src/main/java/runus/runus/board/repository/BoardRepository.java
+++ BackEnd/RunUs/src/main/java/runus/runus/board/repository/BoardRepository.java
@@ -10,52 +10,42 @@ import runus.runus.board.entity.BoardEntity;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 
-    // 게시글 상세보기
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.boardId = :boardId")
     BoardResponseDTO findByIdDetail(@Param("boardId") int boardId);
 
-
-    // 게시글 목록
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.regionId = :regionId AND b.isDeleted = :isDeleted " +
             "ORDER BY b.boardId DESC ")
     Page<BoardResponseDTO> findByRegionId(@Param("regionId") int regionId, @Param("isDeleted") char isDeleted, Pageable pageable);
 
-    // 게시글 목록 - 검색
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.regionId = :regionId AND b.isDeleted = :isDeleted AND b.title LIKE %:title% " +
             "ORDER BY b.boardId DESC ")
     Page<BoardResponseDTO> findByRegionIdByKeyword(@Param("regionId") int regionId, @Param("title") String title, @Param("isDeleted") char isDeleted, Pageable pageable);
 
-    
-    // 달리기 기한 가까운 목록
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.regionId = :regionId AND b.isDeleted = :isDeleted AND b.meetingTime >= CURRENT_TIMESTAMP " +
             "ORDER BY b.meetingTime ASC")
     Page<BoardResponseDTO> findUpcomingMeetings(@Param("regionId") int regionId, @Param("isDeleted") char isDeleted, Pageable pageable);
 
-    // 달리기 기한 가까운 목록 - 검색
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.regionId = :regionId AND b.isDeleted = :isDeleted AND b.meetingTime >= CURRENT_TIMESTAMP AND b.title LIKE %:title% " +
             "ORDER BY b.meetingTime ASC")
     Page<BoardResponseDTO> findUpcomingMeetingsByKeyword(@Param("regionId") int regionId, @Param("title") String title, @Param("isDeleted") char isDeleted, Pageable pageable);
 
-
-    // 달리기 안 끝난 목록
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.regionId = :regionId AND b.isDeleted = :isDeleted AND b.meetingTime >= CURRENT_TIMESTAMP " +
-            "ORDER BY b.boardId desc ")
+            "ORDER BY b.boardId DESC ")
     Page<BoardResponseDTO> findIncompleteMeetings(@Param("regionId") int regionId, @Param("isDeleted") char isDeleted, Pageable pageable);
 
-    // 달리기 안 끝난 목록 - 검색
-    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted) " +
+    @Query("SELECT new runus.runus.board.dto.BoardResponseDTO(b.boardId, b.title, b.content, b.createdAt, b.updatedAt, b.regionId, b.meetingTime, b.meetingDay, COALESCE(u.nickname, 'undefined'), b.isDeleted, u.exp) " +
             "FROM BoardEntity b LEFT JOIN runus.runus.user.entity.User u ON b.userId = u.userId " +
             "WHERE b.regionId = :regionId AND b.isDeleted = :isDeleted AND b.meetingTime >= CURRENT_TIMESTAMP AND b.title LIKE %:title% " +
             "ORDER BY b.boardId DESC ")
