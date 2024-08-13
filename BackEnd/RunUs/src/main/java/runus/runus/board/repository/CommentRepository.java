8146bc6ef93a4310313abbce3a7d3b22938d372d
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-08-13 16:25:58 +0900

    [BE] feat : comment 경험치 추가

--- BackEnd/RunUs/src/main/java/runus/runus/board/repository/CommentRepository.java
+++ BackEnd/RunUs/src/main/java/runus/runus/board/repository/CommentRepository.java
@@ -14,7 +14,7 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Integer>
     List<CommentEntity> findByBoardIdAndIsDeleted(int boardId, char isDeleted, Pageable pageable);
 
     // 댓글 목록 + 닉네임
-    @Query("SELECT new runus.runus.board.dto.CommentResponseDTO(c.commentId, c.boardId, c.parentId, c.userId, c.content, c.createdAt, c.updatedAt, COALESCE(u.nickname, 'undefined')) " +
+    @Query("SELECT new runus.runus.board.dto.CommentResponseDTO(c.commentId, c.boardId, c.parentId, c.userId, c.content, c.createdAt, c.updatedAt, COALESCE(u.nickname, 'undefined'), u.exp) " +
             "FROM CommentEntity c LEFT JOIN runus.runus.user.entity.User u ON c.userId = u.userId " +
             "WHERE c.boardId = :boardId AND c.isDeleted = :isDeleted " +
             "ORDER BY c.boardId ASC ")
