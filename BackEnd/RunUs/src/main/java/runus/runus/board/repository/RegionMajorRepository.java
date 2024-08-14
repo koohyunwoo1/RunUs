0a5718c10950f1dc7d52e22438274a5389798f16
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-08-14 17:45:00 +0900

    [FE] refactor : FE 디렉터리만 구성

--- BackEnd/RunUs/src/main/java/runus/runus/board/repository/RegionMajorRepository.java
+++
@@ -1,7 +0,0 @@
-package runus.runus.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import runus.runus.board.entity.RegionMajor;
-
-public interface RegionMajorRepository extends JpaRepository<RegionMajor, Integer> {
-}
--- BackEnd/RunUs/src/main/java/runus/runus/webSocket/service/ChatService.java
+++
@@ -1,9 +0,0 @@
-package runus.runus.webSocket.service;
-
-import runus.runus.webSocket.dto.ChatRoom;
-import runus.runus.webSocket.dto.PartyRequestDto;
-
-public interface ChatService {
-    ChatRoom createRoom(PartyRequestDto partyRequestDto, String userName);
-    String sendMessage();
-}
